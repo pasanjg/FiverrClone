4b328b3921f594fe781d9d1e2dbaf3e9030ad094
pasanjg <44201430+pasanjg@users.noreply.github.com>
2020-01-23 02:14:55 +0530

    Manage Sales TabBar

--- lib/pages/manage_sales.dart
+++ lib/pages/manage_sales.dart
@@ -8,19 +8,49 @@ class ManageSales extends StatefulWidget {
 
 class _ManageSalesState extends State<ManageSales> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text("Manage Sales"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(FontAwesomeIcons.questionCircle),
-            onPressed: () {},
-          )
-        ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text("Manage Sales"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(FontAwesomeIcons.questionCircle),
+              onPressed: () {},
+            )
+          ],
+          bottom: TabBar(
+            labelColor: Theme.of(context).accentColor,
+            unselectedLabelColor: Colors.grey,
+            tabs: <Widget>[
+              Tab(
+                text: "COMPLETED (102)",
+              ),
+              Tab(
+                text: "CANCELLED (3)",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Text("Completed Orders"),
+            Text("Cancelled Orders"),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
